7f03280e42c8d9b8995bbb47a1257778b3e23fd3
Mitanshu Kumawat <kumawatmitanshu0209@gmail.com>
2025-02-02 19:14:48 +0530

    fixed some bugs in UI of forms

--- client/src/new_components/Error/Error.js
+++ client/src/new_components/Error/Error.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Error = () => {
     return (
-        <div className='w-screen h-screen flex flex-col justify-center items-center'>
-            <div className='text-5xl capitalize'>Error 404: Page not found</div>
-            <div className='text-3xl mt-12'>It's OK to get lost every once in a while...</div>
-            <a href="/" className='hover:underline text-2xl mt-4'>Navigate to Home</a>
+        <div className='w-screen h-screen flex flex-col justify-center items-center bg-[#222831]'>
+            <div className='text-5xl capitalize text-[#EEEEEE]'>Error 404: Page not found</div>
+            <div className='text-3xl mt-12 text-[#EEEEEE]'>It's OK to get lost every once in a while...</div>
+            <a href="/" className='hover:underline text-2xl mt-4 text-[#EEEEEE]'>Navigate to Home</a>
         </div>
     )
 }
