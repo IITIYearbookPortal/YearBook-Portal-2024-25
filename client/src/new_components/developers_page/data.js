bb50be64056ab0fe83aa708078a450d81942ce13
codecv28 <coutcv28@gmail.com>
2026-01-30 23:01:53 +0530

    Add developers page updates and assets

--- client/src/new_components/developers_page/data.js
+++ client/src/new_components/developers_page/data.js
@@ -15,18 +15,25 @@ import img15 from "./images/SaiVarshith.webp"
 import img16 from "./images/Yash.webp"
 import img17 from "./images/Ashish_Donth.webp"
 import img18 from "./images/Mitanshu.webp"
-import img19 from "./images/Sarang.webp"
+//-------------------------------------------------
+import img19 from "./images/anish.webp"
+import img20 from "./images/aviral.webp"
+import img21 from "./images/chetan.webp"
+import img22 from "./images/sahiba.webp"
+import img23 from "./images/sethumadhav.webp"
+import img24 from "./images/vaatsalya.webp"
+import img25 from "./images/veer.webp"
+import img26 from "./images/Sarang.webp"
   
   
   export const Headdata = [
     {
-      name:"Prachi Patil",
-      image:img11,
-      role:" ",
-      github_id:"https://github.com/Prachi-0205",
-      insta_id:"https://www.instagram.com/prachi_patil0205/",
-      linkedin_id:"https://www.linkedin.com/in/prachi-patil-93402725b/",
-
+      name:"Yashasvi",
+      image:img13,
+      role:"Backend",
+      github_id:"https://github.com/Yashasvi1515",
+      insta_id:"https://www.instagram.com/_.yashasvi.__/",
+      linkedin_id:"https://www.linkedin.com/in/yashasvi-shukla-49882030a/",
     }
   ];
 
@@ -70,6 +77,82 @@ import img19 from "./images/Sarang.webp"
 
   export const Voldata = [
     {
+      name:"Aviral Sharma",
+      image:img20,
+      role:"Backend and Frontend",
+      github_id:"https://github.com/AviralSharma11",
+      insta_id:"https://www.instagram.com/aviralsharma.7?igsh=MXVqNHhiNG9ubDMyNQ==",
+      linkedin_id:"https://www.linkedin.com/in/aviral-sharma-b57127299?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      name:"Anish De",
+      image:img19,
+      role:"Backend and Frontend",
+      github_id:"https://github.com/Anish-gthub",
+      insta_id:"https://www.instagram.com/ani_sh_2005?igsh=MXA4cTIzaGh4eDVu",
+      linkedin_id:"https://www.linkedin.com/in/anish-de-601383375?utm_source=share_via&utm_content=profile&utm_medium=member_android",
+    },
+    {
+      name:"Bhukya Sethumadhavnaik",
+      image:img23,
+      role:"Frontend",
+      github_id:"https://github.com/Sethu-madhav17",
+      insta_id:"https://www.instagram.com/sethumadhav_19?igsh=eWZ4bG1kNGtwNTll",
+      linkedin_id:"https://www.linkedin.com/in/bhukya-sethu-madhav-naik-467b96366?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    
+    {
+      name:"Chetan Verma",
+      image:img21,
+      role:"Frontend",
+      github_id:"https://github.com/codecv28",
+      insta_id:"https://www.instagram.com/chetanverma_28/",
+      linkedin_id:"www.linkedin.com/in/chetanverma2807",
+    },
+    {
+      name:"Sahiba Joshi",
+      image:img22,
+      role:"Backend and Frontend",
+      github_id:"https://github.com/SA0806",
+      insta_id:"https://www.instagram.com/sahiba_joshi.06?igsh=MXQ2NGdnd29tcmNvNQ==",
+      linkedin_id:"https://www.linkedin.com/in/sahiba-joshi",
+    },
+    {
+      name:"Sarang Thakare",
+      image:img26,
+      role:"Frontend",
+      github_id:"https://github.com/SARANGVT",
+      insta_id:"",
+      linkedin_id:"https://www.linkedin.com/in/sarang-thakare-101866229",
+    },
+    {
+      name:"Vaatsalya Sahu",
+      image:img24,
+      role:"Frontend",
+      github_id:"https://github.com/Svaatsalya",
+      insta_id:"https://www.instagram.com/vaatsalya_s/",
+      linkedin_id:"www.linkedin.com/in/vaatsalya-sahu-28274031b",
+    },
+    {
+      name:"Veer Doria",
+      image:img25,
+      role:"Backend",
+      github_id:"https://github.com/VeerDoria",
+      insta_id:"https://www.instagram.com/veerdoria/",
+      linkedin_id:"https://www.linkedin.com/in/veer-doria-16567b330/",
+    },
+  ];
+  export const pastMembers = [
+    {
+      name:"Prachi Patil",
+      image:img11,
+      role:" ",
+      github_id:"https://github.com/Prachi-0205",
+      insta_id:"https://www.instagram.com/prachi_patil0205/",
+      linkedin_id:"https://www.linkedin.com/in/prachi-patil-93402725b/",
+
+    },
+    {
       name:"Sai Varshith",
       image:img15,
       role:"Backend",
@@ -101,13 +184,6 @@ import img19 from "./images/Sarang.webp"
       github_id:"https://github.com/yashvardhan2006",
       insta_id:"https://www.instagram.com/vedantdms/",
       linkedin_id:" https://www.linkedin.com/in/yash-vardhan-solanki-424384224",
-    },{
-      name:"Yashasvi",
-      image:img13,
-      role:"Frontend",
-      github_id:"https://github.com/Yashasvi1515",
-      insta_id:"https://www.instagram.com/_.yashasvi.__/",
-      linkedin_id:"https://www.linkedin.com/in/yashasvi-shukla-49882030a/",
     },
     {
       name:"Mitanshu Kumawat",
@@ -117,97 +193,88 @@ import img19 from "./images/Sarang.webp"
       insta_id:"https://www.instagram.com/mitanshu8905/",
       linkedin_id:"https://www.linkedin.com/in/mitanshu-kumawat-015a28283/",
     },
-    {
-      name:"Sarang Thakare",
-      image:img19,
-      role:"Frontend",
-      github_id:"https://github.com/SARANGVT",
-      insta_id:"",
-      linkedin_id:"https://www.linkedin.com/in/sarang-thakare-101866229",
-    },
-  ];
-  export const pastMembers = [
-    {
-      name:"Tarun Balaji",
-      image:img1,
-      role:"Backend and Frontend",
-      github_id:"https://github.com/Ghost4681",
-      insta_id:"https://www.instagram.com/tarunbalaji2003/",
-      linkedin_id:"https://www.linkedin.com/in/a-tarun-balaji/",
+    
+    // {
+    //   name:"Tarun Balaji",
+    //   image:img1,
+    //   role:"Backend and Frontend",
+    //   github_id:"https://github.com/Ghost4681",
+    //   insta_id:"https://www.instagram.com/tarunbalaji2003/",
+    //   linkedin_id:"https://www.linkedin.com/in/a-tarun-balaji/",
       
-    },
-    {
-      name:"Harsh Soni",
-      image:img10,
-      role:"Backend",
-      github_id:"https://github.com/SoniHarsh1",
-      insta_id:"https://www.instagram.com/harsh.soni_1/",
-      linkedin_id:"https://www.linkedin.com/in/harsh-soni15/",
-    },
-    {
-      name:"Aditi Ganvir",
-      image:img12,
-      role:"Backend",
-      github_id:"https://github.com/aditiganvir28",
-      insta_id:"https://www.instagram.com/ganviraditi/",
-      linkedin_id:"https://www.linkedin.com/in/aditi-ganvir-216336232/",
-    },
-    {
-      name:"Ayush Awasthi",
-      image:img6,
-      role:"Backend",
-      github_id:"https://github.com/ayushawasthi24",
-      insta_id:"https://www.instagram.com/ayush__awasthi__/",
-      linkedin_id:"https://www.linkedin.com/in/ayush-awasthi-a232b8254/",
-    },
-    {
-      name:"Adarsh Ranjan",
-      image:img2,
-      role:"Backend",
-      github_id:"https://github.com/aadarsh2904",
-      insta_id:"https://www.instagram.com/solanki_aadarsh_/",
-      linkedin_id:"https://www.linkedin.com/in/aadarsh-ranjan-552080260/",
-    },
-    {
-      name:"Jayesh More",
-      image:img7,
-      role:"Frontend",
-      github_id:"https://github.com/JayeshM-01",
-      insta_id:"https://www.instagram.com/jayesh_m2201/",
-      linkedin_id:"https://www.linkedin.com/in/jayesh-more-1ba795255/",
-    },
-    {
-      name:"Naveen SH",
-      image:img3,
-      role:"Frontend",
-      github_id:"https://github.com/NaveenSh7",
-      insta_id:"https://www.instagram.com/naveen_sh07/",
-      linkedin_id:"https://www.linkedin.com/in/naveen-s-h-3545b8262/",
-    },
-    {
-      name:"Parth Deshmukh",
-      image:img5,
-      role:"Backend",
-      github_id:"https://github.com/parth20031",
-      insta_id:"https://www.instagram.com/parth_deshmukh96?igsh=c3N0dzdlMGVkenRs",
-      linkedin_id:"https://www.linkedin.com/in/parth-deshmukh-b01044277/",
-    },
-    {
-      name:"Samruddhi Patil",
-      image:img8,
-      role:"Frontend",
-      github_id:"https://github.com/Samruddhi788",
-      insta_id:"https://www.instagram.com/samruddhirpatil788/",
-      linkedin_id:"https://www.linkedin.com/in/samruddhi-patil-0b1a5025b/",
-    },
-    {
-      name:"Vedant Dinkar",
-      image:img9,
-      role:"Backend",
-      github_id:"https://github.com/Vedant-Dinkar",
-      insta_id:"https://www.instagram.com/vedantdms/",
-      linkedin_id:"https://www.linkedin.com/in/vedant-dinkar-a6a4301b9/",
-    },
+    // },
+    // {
+    //   name:"Harsh Soni",
+    //   image:img10,
+    //   role:"Backend",
+    //   github_id:"https://github.com/SoniHarsh1",
+    //   insta_id:"https://www.instagram.com/harsh.soni_1/",
+    //   linkedin_id:"https://www.linkedin.com/in/harsh-soni15/",
+    // },
+    // {
+    //   name:"Aditi Ganvir",
+    //   image:img12,
+    //   role:"Backend",
+    //   github_id:"https://github.com/aditiganvir28",
+    //   insta_id:"https://www.instagram.com/ganviraditi/",
+    //   linkedin_id:"https://www.linkedin.com/in/aditi-ganvir-216336232/",
+    // },
+    // {
+    //   name:"Ayush Awasthi",
+    //   image:img6,
+    //   role:"Backend",
+    //   github_id:"https://github.com/ayushawasthi24",
+    //   insta_id:"https://www.instagram.com/ayush__awasthi__/",
+    //   linkedin_id:"https://www.linkedin.com/in/ayush-awasthi-a232b8254/",
+    // },
+    // {
+    //   name:"Adarsh Ranjan",
+    //   image:img2,
+    //   role:"Backend",
+    //   github_id:"https://github.com/aadarsh2904",
+    //   insta_id:"https://www.instagram.com/solanki_aadarsh_/",
+    //   linkedin_id:"https://www.linkedin.com/in/aadarsh-ranjan-552080260/",
+    // },
+    // {
+    //   name:"Jayesh More",
+    //   image:img7,
+    //   role:"Frontend",
+    //   github_id:"https://github.com/JayeshM-01",
+    //   insta_id:"https://www.instagram.com/jayesh_m2201/",
+    //   linkedin_id:"https://www.linkedin.com/in/jayesh-more-1ba795255/",
+    // },
+    // {
+    //   name:"Naveen SH",
+    //   image:img3,
+    //   role:"Frontend",
+    //   github_id:"https://github.com/NaveenSh7",
+    //   insta_id:"https://www.instagram.com/naveen_sh07/",
+    //   linkedin_id:"https://www.linkedin.com/in/naveen-s-h-3545b8262/",
+    // },
+    // {
+    //   name:"Parth Deshmukh",
+    //   image:img5,
+    //   role:"Backend",
+    //   github_id:"https://github.com/parth20031",
+    //   insta_id:"https://www.instagram.com/parth_deshmukh96?igsh=c3N0dzdlMGVkenRs",
+    //   linkedin_id:"https://www.linkedin.com/in/parth-deshmukh-b01044277/",
+    // },
+    // {
+    //   name:"Samruddhi Patil",
+    //   image:img8,
+    //   role:"Frontend",
+    //   github_id:"https://github.com/Samruddhi788",
+    //   insta_id:"https://www.instagram.com/samruddhirpatil788/",
+    //   linkedin_id:"https://www.linkedin.com/in/samruddhi-patil-0b1a5025b/",
+    // },
+    // {
+    //   name:"Vedant Dinkar",
+    //   image:img9,
+    //   role:"Backend",
+    //   github_id:"https://github.com/Vedant-Dinkar",
+    //   insta_id:"https://www.instagram.com/vedantdms/",
+    //   linkedin_id:"https://www.linkedin.com/in/vedant-dinkar-a6a4301b9/",
+    // },
   ]
 
 
